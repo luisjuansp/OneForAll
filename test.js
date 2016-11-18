949af4293697515fa06fb86fb3562b9e2a59e4ca
EmilioFlores <emilio_wassa@hotmail.com>
2016-11-18 09:43:34 -0600

    image test

--- test.js
+++ test.js
@@ -12,6 +12,11 @@ oneForAll.recieveImage = function(message){
 	request.post({url:'http://159.203.78.76/api/receiveimage', json : {message: message}}, function optionalCallback(err, httpResponse, body) {
 	});
 	oneForAll.sendImage([message]);
+	require('vision.js')(req.url, function (text) {
+		message.text = text;
+		oneForAll.sendMessage([message]);
+		//send({text: text});
+	});
 }
 
 oneForAll.recieveAudio = function(message){
