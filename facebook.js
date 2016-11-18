074311e8ed9e4d6609f48946438dcff52f6604c7
EmilioFlores <emilio_wassa@hotmail.com>
2016-11-17 18:22:28 -0600

    test

--- facebook.js
+++ facebook.js
@@ -37,6 +37,7 @@ app.listen(app.get('port'), function() {
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging;
     let sender = event.sender.id
+    console.log(sender);
     sendTextMessage(sender, sender);
     /*
     for (let i = 0; i < messaging_events.length; i++) {
@@ -175,9 +176,10 @@ app.post("/send", function(req, res){
 
 app.get("/send", function(req, res){    
 
-    console.log(req);
-    res.send('GET')
     
+    res.send('GET')
+    var sender = "10157758880380472";
+    sendTextMessage(sender,sender);
     
 	//bot.postMessage("D25K85JSX", req);
 	
