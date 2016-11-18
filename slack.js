f82a5d08e5f0239a65924ec0f09089bf5dbe41f8
Luis Juan Sanchez <luisjuansp@gmail.com>
2016-11-17 19:59:17 -0800

    init slackbot

---
+++ slack.js
@@ -0,0 +1,21 @@
+var SlackBot = require('slackbots');
+var config = require('./config.json');
+
+var bot = new SlackBot({
+    token: config.slack_token, 
+    name: config.slack_name
+});
+
+
+bot.on('start', function() {
+	//bot.postMessageToChannel('general', 'meow!');
+	//bot.postMessageToUser('user_name', 'meow!');
+	//bot.postMessageToUser('user_name', 'meow!');
+	//bot.postMessageToGroup('private_group', 'meow!');
+});
+
+bot.on('message', function(data){
+	if(data.type == "message")){
+		postMessageToChannel(data.channel, data);
+	}
+});
