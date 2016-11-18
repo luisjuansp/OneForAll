51914131e7bf29e08a305d79d5f3bc1dcbaf578a
Luis Juan Sanchez <luisjuansp@gmail.com>
2016-11-17 20:11:48 -0800

    slackbot

--- slack.js
+++ slack.js
@@ -2,13 +2,13 @@ var SlackBot = require('slackbots');
 var config = require('./config.json');
 
 var bot = new SlackBot({
-    token: config.slack_token, 
-    name: config.slack_name
+    token: 'xoxb-106383320373-Y6DKxUMZbx6EEl9bCUEvYi4S', 
+    name: 'oneforall'
 });
 
 
 bot.on('start', function() {
-	//bot.postMessageToChannel('general', 'meow!');
+	bot.postMessageToChannel('general', 'meow!');
 	//bot.postMessageToUser('user_name', 'meow!');
 	bot.postMessageToUser('lowes', 'meow!');
 	//bot.postMessageToGroup('private_group', 'meow!');
