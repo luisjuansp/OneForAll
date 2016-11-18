6e108bdfa80aab57519f0c2fc96105e5983f0430
EmilioFlores <emilio_wassa@hotmail.com>
2016-11-17 19:39:50 -0600

    commented tst.js

--- test.js
+++ test.js
@@ -2,10 +2,12 @@ var oneForAll = require("./main.js");
 
 oneForAll.recieveMessage = function(data){
 	oneForAll.sendMessage(data);
-	oneForAll.sendImage({
+	
+	/*oneForAll.sendImage({
 			id : data.id,
 			image : "https://scontent.xx.fbcdn.net/v/t34.0-12/15129935_10155401879574428_1448012768_n.png?oh=fd144d7e8d888ce5233a8a25cfeacc09&oe=58318C0E"
 		 });
+	*/	
 }
 
 oneForAll.listen();
