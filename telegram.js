063f30ac3a1faf959e4b05910a51607687bb5be0
EmilioFlores <emilio_wassa@hotmail.com>
2016-11-18 04:51:56 -0600

    telegram done

--- telegram.js
+++ telegram.js
@@ -36,7 +36,7 @@ bot.on('message', function (msg) {
         var photo = msg.photo[0].file_id;
         var data = {id : chatId, photo : photo };
         //bot.sendPhoto(chatId, photo );
-        exports.recieveMessage(data);
+        exports.recieveImage(data);
   }
   else if (msg.video) {
         var video = msg.video.file_id;
