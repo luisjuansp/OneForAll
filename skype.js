43ffcdb811157bfc03bafaa14957450ba5d35c4f
EmilioFlores <emilio_wassa@hotmail.com>
2016-11-18 02:28:41 -0600

    test

--- skype.js
+++ skype.js
@@ -68,6 +68,42 @@ module.exports = function(app) {
 
     */
 
+    bot.dialog('/', [
+        function (session) {
+            session.beginDialog('/ensureProfile', session.userData.profile);
+        },
+        function (session, results) {
+            session.userData.profile = results.response;
+            session.send('Hello %(name)s! I love %(company)s!', session.userData.profile);
+        }
+    ]);
+    bot.dialog('/ensureProfile', [
+        function (session, args, next) {
+            session.dialogData.profile = args || {};
+            if (!session.dialogData.profile.name) {
+                builder.Prompts.text(session, "What's your name?");
+            } else {
+                next();
+            }
+        },
+        function (session, results, next) {
+            if (results.response) {
+                session.dialogData.profile.name = results.response;
+            }
+            if (!session.dialogData.profile.company) {
+                builder.Prompts.text(session, "What company do you work for?");
+            } else {
+                next();
+            }
+        },
+        function (session, results) {
+            if (results.response) {
+                session.dialogData.profile.company = results.response;
+            }
+            session.endDialogWithResult({ response: session.dialogData.profile });
+        }
+    ]);
+
 
 //module.exports = exports;
  return exports;
