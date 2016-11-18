c8c7c9a28c43bd795145a189002afb71d1de3518
EmilioFlores <emilio_wassa@hotmail.com>
2016-11-17 19:31:54 -0600

    send image

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var oneForAll = require("./main.js");
 
 oneForAll.recieveMessage = function(data){
 	oneForAll.sendMessage(data);
-	oneForAll.sendImage(data.sender, "https://scontent.xx.fbcdn.net/v/t34.0-12/15129935_10155401879574428_1448012768_n.png?oh=fd144d7e8d888ce5233a8a25cfeacc09&oe=58318C0E");
+	oneForAll.sendImage(data.id, "https://scontent.xx.fbcdn.net/v/t34.0-12/15129935_10155401879574428_1448012768_n.png?oh=fd144d7e8d888ce5233a8a25cfeacc09&oe=58318C0E");
 }
 
 oneForAll.listen();
